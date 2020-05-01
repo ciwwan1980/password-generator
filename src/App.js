8f955db1ee5d9b8b6593397275ff09082a866043
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-01 23:30:34 +0200

    submit-function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import PassGenerator from "./components/PassGenerator"
-import 'materialize-css/dist/css/materialize.min.css';
+import Form from "./components/Form"
+
 
 function App() {
   return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,52 @@
 import React,{useState} from 'react'
 
-export default function Form {
+export default function Form() {
 
     const [password,setPassword]=useState("")
     const [passwordLength,setPasswordLength]=useState(12)
      const [big,setBig]=useState(true)
       const [special,setSpecial]=useState(true)
 
+   let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+       let bigCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       let specialChar = "!§$%&?";
+    
+
+const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(Math.floor(Math.random()*characters.length))
+    createPassword(passwordLength)
+}
+
+const createPassword=(length)=>{
+    let ourPassword="";
+    let allCharacters=characters;
+    console.log(allCharacters, ".......allcharacters")
+    console.log(characters,"characters........")
+    if(big){
+       allCharacters =allCharacters + bigCase
+    }
+    if(special){
+        allCharacters =allCharacters + specialChar
+    }
+    for(let i=0;i<length-1;i++){
+        console.log(allCharacters)
+        let randomNumber= Math.floor(Math.random()*allCharacters.length)
+        console.log("random....",randomNumber)
+        
+        ourPassword=ourPassword + allCharacters[randomNumber]
+        console.log("ourpassword........", ourPassword)
+    }
+    setPassword(ourPassword)
+}
+
     return (
         <div>    
-    <form onSubmit={this.handleSubmit} className="flex">
+    <form onSubmit={handleSubmit} className="flex">
  
     <fieldset>
         <label>Result: </label>
-        <input name="password" value={this.state.password} readOnly/>
+        <input name="password" value={password} readOnly/>
         <button>Generate Password</button>
     </fieldset>
 
@@ -23,16 +56,16 @@ export default function Form {
             
             <label>
                 Passwordlength
-                <input name="passwordLength" value={this.state.passwordLength} onChange={this.handleChange}/>
+                <input name="passwordLength" value={passwordLength} />
             </label>
             
             <label>
-            <input type="checkbox" name="big" checked={this.state.big} onChange={this.handleCheck} />
+            <input type="checkbox" name="big" checked={big}  />
                 useMixedCase
             </label>
             
             <label>
-                <input type="checkbox" name="special" checked={this.state.special} onChange={this.handleCheck}/>
+                <input type="checkbox" name="special" checked={special} />
                 specialCase
             </label>
     </fieldset>
